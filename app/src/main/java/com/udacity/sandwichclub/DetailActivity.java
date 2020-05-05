da3177586cb857fe4c3b42eaa571315fe1eb3082
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2020-05-05 22:37:32 +0200

    Implement populateUI function

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +17,14 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private Sandwich sandwich;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ImageView sandwichPictureIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -35,7 +39,7 @@ public class DetailActivity extends AppCompatActivity {
 
             String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
             String json = sandwiches[position];
-            Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+            sandwich = JsonUtils.parseSandwichJson(json);
             if (sandwich == null) {
                 // Sandwich data unavailable
                 closeOnError();
@@ -45,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
             populateUI();
             Picasso.with(this)
                     .load(sandwich.getImage())
-                    .into(ingredientsIv);
+                    .into(sandwichPictureIv);
 
             setTitle(sandwich.getMainName());
         }
@@ -57,6 +61,64 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        TextView alsoKnownAsLabel = findViewById(R.id.also_known_label);
+        TextView placeOfOriginLabel = findViewById(R.id.origin_label);
+        TextView descriptionLabel = findViewById(R.id.description_label);
+        TextView ingredientsLabel = findViewById(R.id.ingredients_label);
 
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        TextView placeOfOriginTv = findViewById(R.id.origin_tv);
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+
+        if (sandwich.getAlsoKnownAs() != null) {
+            for (String s : sandwich.getAlsoKnownAs()) {
+                StringBuilder alsoKnown = new StringBuilder();
+                if (!s.isEmpty()) {
+                    alsoKnownAsLabel.setVisibility(View.VISIBLE);
+                    alsoKnownAsTv.setVisibility(View.VISIBLE);
+                    alsoKnown.append(s).append("\n");
+                }
+                String alsoKnownString = alsoKnown.toString().trim();
+                alsoKnownAsTv.setText(alsoKnownString);
+            }
+        } else {
+            alsoKnownAsLabel.setVisibility(View.GONE);
+            alsoKnownAsTv.setVisibility(View.GONE);
+        }
+
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+            placeOfOriginLabel.setVisibility(View.VISIBLE);
+            placeOfOriginTv.setVisibility(View.VISIBLE);
+            placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            placeOfOriginLabel.setVisibility(View.GONE);
+            placeOfOriginTv.setVisibility(View.GONE);
+        }
+
+        if (sandwich.getDescription() != null && !sandwich.getDescription().isEmpty()) {
+            descriptionLabel.setVisibility(View.VISIBLE);
+            descriptionTv.setVisibility(View.VISIBLE);
+            descriptionTv.setText(sandwich.getDescription());
+        } else {
+            descriptionLabel.setVisibility(View.GONE);
+            descriptionTv.setVisibility(View.GONE);
+        }
+
+        if (sandwich.getIngredients() != null) {
+            for (String s : sandwich.getIngredients()) {
+                StringBuilder ingredients = new StringBuilder();
+                if (!s.isEmpty()) {
+                    ingredientsLabel.setVisibility(View.VISIBLE);
+                    ingredientsTv.setVisibility(View.VISIBLE);
+                    ingredients.append(s).append("\n");
+                }
+                String ingredientsString = ingredients.toString().trim();
+                ingredientsTv.append(ingredientsString);
+            }
+        } else {
+            ingredientsLabel.setVisibility(View.GONE);
+            ingredientsTv.setVisibility(View.GONE);
+        }
     }
 }
