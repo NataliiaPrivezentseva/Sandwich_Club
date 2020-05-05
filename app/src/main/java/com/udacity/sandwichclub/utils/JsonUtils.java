c262247bc8b6823a70a4438efbca5cc060bb315c
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2020-05-05 18:14:50 +0200

    Implement parseSandwichJson function

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,71 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        try {
+            sandwich = new Sandwich();
+
+            JSONObject sandwichJson = new JSONObject(json);
+
+            JSONObject name = sandwichJson.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            if (mainName != null && !mainName.isEmpty()) {
+                sandwich.setMainName(mainName);
+            }
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            if (alsoKnownAsArray != null && alsoKnownAsArray.length() > 0) {
+                ArrayList<String> alsoKnownAs = new ArrayList<>();
+                for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                    String element = alsoKnownAsArray.getString(i);
+                    if (element != null && !element.isEmpty()) {
+                        alsoKnownAs.add(element);
+                    }
+                }
+                if (!alsoKnownAs.isEmpty()) {
+                    sandwich.setAlsoKnownAs(alsoKnownAs);
+                }
+            }
+
+            String placeOfOrigin = sandwichJson.getString("placeOfOrigin");
+            if (placeOfOrigin != null && !placeOfOrigin.isEmpty()) {
+                sandwich.setPlaceOfOrigin(placeOfOrigin);
+            }
+
+            String description = sandwichJson.getString("description");
+            if (description != null && !description.isEmpty()){
+                sandwich.setDescription(description);
+            }
+
+            String image = sandwichJson.getString("image");
+            if (image != null && !image.isEmpty()){
+                sandwich.setImage(image);
+            }
+
+            JSONArray ingredientsArray = sandwichJson.getJSONArray("ingredients");
+            if (ingredientsArray != null && ingredientsArray.length() > 0) {
+                ArrayList<String> ingredients = new ArrayList<>();
+                for (int i = 0; i < ingredientsArray.length(); i++) {
+                    String element = ingredientsArray.getString(i);
+                    if (element != null && !element.isEmpty()) {
+                        ingredients.add(element);
+                    }
+                }
+                if (!ingredients.isEmpty()) {
+                    sandwich.setIngredients(ingredients);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
