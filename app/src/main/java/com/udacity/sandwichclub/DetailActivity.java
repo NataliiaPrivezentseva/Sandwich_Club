7bff0d972fce92d433bbae6daa5b88db5a20a3b9
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2020-05-06 10:37:53 +0200

    Add progress bar to be shown while dowloading a picture and add default sandwich image

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
@@ -25,6 +27,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView sandwichPictureIv = findViewById(R.id.image_iv);
+        final ProgressBar progressBar = findViewById(R.id.progress_bar);
+        final ImageView defaultImageIv = findViewById(R.id.default_image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -47,12 +51,30 @@ public class DetailActivity extends AppCompatActivity {
             }
 
             populateUI();
+            sandwichPictureIv.setScaleType(ImageView.ScaleType.CENTER_CROP);
             Picasso.with(this)
                     .load(sandwich.getImage())
-                    .into(sandwichPictureIv);
+                    .into(sandwichPictureIv, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            progressBar.setVisibility(View.GONE);
+                            defaultImageIv.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onError() {
+                            progressBar.setVisibility(View.GONE);
+                            defaultImageIv.setVisibility(View.VISIBLE);
+                            showError();
+                        }
+                    });
 
             setTitle(sandwich.getMainName());
         }
+    }
+
+    private void showError() {
+        Toast.makeText(this, R.string.image_error_message, Toast.LENGTH_SHORT).show();
     }
 
     private void closeOnError() {
